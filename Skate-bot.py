3d8de28d6a6843484f241c8cbad9da7a479bb08f
r3vrse <jakepampling2@gmail.com>
2020-12-01 14:26:55 +0000

    New files

--- Skate-bot.py
+++ Skate-bot.py
@@ -3,13 +3,17 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import json
 import random
+import time
 
 with open("Skate-bot-settings.json", "r") as f:
 	my_dict = json.load(f)
 
+intents = discord.Intents.default()
+intents.members = True
+
 #Sets the bots command prefix and also states what channel it will be active in.
 
-bot = commands.Bot(command_prefix = my_dict['command_prefix'])
+bot = commands.Bot(command_prefix = my_dict['command_prefix'], intents=intents)
 channel = bot.get_channel("781631030972514335")
 
 #Notifies you that the bot is ready in the terminal.
@@ -86,6 +90,102 @@ async def ping(ctx):
 async def SOTW(ctx):
 
 	await ctx.send("\tSpot of the week: Easy Day High School!", file=discord.File("EasyDay.png"))
+
+# Game of skate
+
+
+@bot.command()
+async def skate(ctx):
+
+	beginner_line = random.choice(beginnerTricks) + ' ~ ' + random.choice(a_grinds)
+	beginner_line2 = random.choice(beginnerTricks) + ' ~ ' + random.choice(b_manuals)
+
+	beginnerskate = random.choice([beginner_line, beginner_line2])
+
+	await ctx.send("Welcome to the game of skate with a twist!")
+	time.sleep(3)
+	await ctx.send("\nWhat is your name: ")
+	time.sleep(3)
+
+	response = await bot.wait_for('message')
+	name = str(response.content)
+
+	await ctx.send(f"\nWelcome to the game {name}!")
+	time.sleep(3)
+	await ctx.send("\nWhich difficulty would you like: \n - beginner \n - intermediate \n - advanced")
+	time.sleep(3)
+
+	choice = await bot.wait_for('message')
+	difficulty = str(choice.content)
+
+
+	if difficulty == 'beginner':
+
+		await ctx.send(f"Here is your first trick, try not to fail: {beginnerskate}")
+		time.sleep(3)
+		await ctx.send('Did you complete the trick first try: ')
+
+		response2 = await bot.wait_for('message')
+		answer = str(response2.content)
+
+		if answer == 'yes':
+
+			beginner_line = random.choice(beginnerTricks) + ' ~ ' + random.choice(a_grinds)
+			beginner_line2 = random.choice(beginnerTricks) + ' ~ ' + random.choice(b_manuals)
+
+			beginnerskate2 = random.choice([beginner_line, beginner_line2])
+
+			await ctx.send('Well done! Your points: 1!')
+			time.sleep(3)
+			await ctx.send(f'Here\'s another trick: {beginnerskate2}')
+			time.sleep(3)
+			await ctx.send('Did you complete the trick: ')
+
+			response3 = await bot.wait_for('message')
+			answer2 = str(response3.content)
+
+			if answer2 == 'yes':
+
+				beginner_line = random.choice(beginnerTricks) + ' ~ ' + random.choice(a_grinds)
+				beginner_line2 = random.choice(beginnerTricks) + ' ~ ' + random.choice(b_manuals)
+
+				beginnerskate3 = random.choice([beginner_line, beginner_line2])
+
+				await ctx.send('Well done! Your points: 2!')
+				time.sleep(3)
+				await ctx.send(f'Here\'s another trick: {beginnerskate3}')
+				time.sleep(3)
+				await ctx.send('Did you complete the trick: ')
+
+				response4 = await bot.wait_for('message')
+				answer3 = str(response4.content)
+
+				if answer3 == 'yes':
+
+					beginner_line = random.choice(beginnerTricks) + ' ~ ' + random.choice(a_grinds)
+					beginner_line2 = random.choice(beginnerTricks) + ' ~ ' + random.choice(b_manuals)
+
+					beginnerskate4 = random.choice([beginner_line, beginner_line2])
+
+					await ctx.send('Well done! Your points: 3!')
+					time.sleep(3)
+					await ctx.send(f'Here\'s another trick: {beginnerskate4}')
+					time.sleep(3)
+					await ctx.send('Did you complete the trick: ')
+
+					response5 = await bot.wait_for('message')
+					answer4 = str(response5.content)
+
+					if answer4 == 'yes':
+
+						await ctx.send('Well done! Your total points: ')
+						time.sleep(3)
+						await ctx.send(f'You win, {name}!')
+
+
+
+
+
 
 # This shows the team roster
 
